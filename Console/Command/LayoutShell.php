de51a132618231b61c4e65e45b626d3989a99152
Florian Nitschmann <f.nitschmann@googlemail.com>
2012-10-11 09:59:58 +0200

    Created LayoutShell

---
+++ Console/Command/LayoutShell.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Layout Shell Script for CakePHP Console to create custom layout files based on Twitter Bootstrap  
+ *
+ * Copyright 2012, visionred (http://visionred.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author 		  Florian Nitschmann (f.nitschmann@visionred.org) 
+ * @copyright     Copyright 2012, visionred (http://visionred.org)
+ * @link          http://visionred.org, visionred Web Solutions
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('File', 'Utility'); 
+App::uses('Folder', 'Utility');
+
+class LayoutShell extends AppShell {
+
+	/*
+	 * main() - The main function for the LayoutShell 
+	 *
+	 * @access public
+	 * @return bool 
+	 */
+	public function main() {
+
+	}
+
+}
+
